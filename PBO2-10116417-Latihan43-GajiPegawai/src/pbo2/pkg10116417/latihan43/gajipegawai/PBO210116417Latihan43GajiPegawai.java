3cba049de777f7e8f9879b061c19a6398d5a0f93
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-15 11:04:34 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan43-GajiPegawai/src/pbo2/pkg10116417/latihan43/gajipegawai/PBO210116417Latihan43GajiPegawai.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan43.gajipegawai;
+
+/**
+ *
+ * @author 
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM MENAMPILKAN GAJI PEGAWAI
+ */
+public class PBO210116417Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+    
+}
